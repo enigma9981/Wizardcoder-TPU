cc60314ddfc671a1a887317266585146c1b9fac4
enigma9981 <yixin.wang@sophgo.com>
2024-01-17 21:12:02 +0800

    Add tests

--- demo/bin/wizardcoder.cc
+++ demo/bin/wizardcoder.cc
@@ -21,8 +21,17 @@ int main(int argc, char** argv) {
 
     std::string input_str;
 
-    while (1) {
-        std::getline(std::cin, input_str);
+    auto inputs = std::vector<const char*>{
+            "Write a Python code to count 1 to 10.",
+            "Write a Jave code to sum 1 to 10.",
+            "Write a Rust code to implement SCC algorithm"
+    };
+
+    int cnt = 0;
+    while (cnt < inputs.size()) {
+        // std::getline(std::cin, input_str);
+        input_str = inputs[cnt++];
+        std::cout << input_str << '\n' << std::flush;
         // input_str = "Write a Python code to count 1 to 10.";
         std::cout << FRED("WizardCoder-15B-V1.0-BM1684X: ") << std::flush;
         auto prompt = model->build_prompt(input_str);
